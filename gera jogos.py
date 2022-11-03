190c164081d7128a7de58022057327c92f699973
Carlos Escorcio <caejr44@outlook.com>
2022-11-03 20:55:25 -0300

    update

--- gera jogos.py
+++ gera jogos.py
@@ -1,14 +1,29 @@
+import os
 import time
 import random
 import sys
+
+from sqlalchemy import true
+
 e = 1
-for e in range(e+5):
+os.system('clear')
+while True:
+    try:
+        jogos = int(input("quantos jogos quer fazer"))
+        break
+    except:
+        print("apenas numeros")
+
+for e in range(e+jogos-1):
     print()
-    print("jogo" + str(e))
+    print("jogo" + str(e+1))
     for i in range(6):
         r = random.randint(1, 60)
-        time.sleep(2)
+        time.sleep(0.01)
         sys.stdout.write(str(r))
         valor = i
         if valor != 5:
-           sys.stdout.write("-")
+            sys.stdout.write("-")
+        else:
+            print()
+            print("fim de jogatina")
