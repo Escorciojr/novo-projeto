5c458d67272660f7acdee8086436eff4109355ab
Carlos Escorcio <caejr44@outlook.com>
2022-11-11 21:00:57 -0300

    update

--- beer.py
+++ beer.py
@@ -1,8 +1,8 @@
 word = "bootles"
-for beer_num in range(50, 0, -1):
+for beer_num in range(50, 0, -2):
     print(beer_num, word, "TESTE")
-    if beer_num ==  1:
+    if beer_num == 1:
         print("TA LISO")
     else:
-        print("tomando mais")  
-        print()  
+        print("tomando mais")
+        print()
