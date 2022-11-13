f4cc67b7cce2435b4b7d4b5359478dbf3443febd
Carlos Escorcio <caejr44@outlook.com>
2022-11-13 19:00:57 -0300

    update

--- vogais.py
+++ vogais.py
@@ -1,8 +1,8 @@
 vogais = ['a', 'e', 'i', 'o', 'u']
 achei = []
 word = input("Digite seu Nome")
-    for letra in word:
-        if letra in vogais:
-            if letra not in achei:
+for letra in word:
+    if letra in vogais:
+        if letra not in achei:
             achei.append(letra)
 print(achei)
